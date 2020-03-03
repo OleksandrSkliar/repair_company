c9ee5910825a8df5b49435edc17b1d04bd31f600
Aleksandr Skliar <sklarsanja@gmail.com>
2020-03-03 16:28:56 +0300

    feat: add header in index.html

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
-const gulp = require('gulp');
-const browserSync = require('browser-sync').create();
+const gulp = require("gulp");
+const browserSync = require("browser-sync").create();
+const rename = require("gulp-rename");
+const cssmin = require("cssmin");
 
-gulp.task('browser-sync', function () {
+gulp.task("browser-sync", function() {
   browserSync.init({
     server: {
       baseDir: "./src"
     }
   });
-  gulp.watch("./src/*.html").on('change', browserSync.reload);
+  gulp.watch("./src/*.html").on("change", browserSync.reload);
 });
