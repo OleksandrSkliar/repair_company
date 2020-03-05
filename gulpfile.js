8e418ffa926fc06ae1ea8ad0193b8953462ccaac
Aleksandr Skliar <sklarsanja@gmail.com>
2020-03-05 21:20:07 +0300

    feat: add fonts and added fonts to the project

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,8 @@ const {
   watch
 } = require("gulp");
 const browserSync = require("browser-sync").create();
-const sass = require('gulp-sass');
+const sass = require("gulp-sass");
+const autoprefixer = require("gulp-autoprefixer");
 
 function bs() {
   serveSass();
@@ -15,16 +16,21 @@ function bs() {
   });
   watch("./src/*.html").on("change", browserSync.reload);
   watch("./src/sass/**/*.sass", serveSass);
+  watch("./src/sass/**/*.scss", serveSass);
   watch("./src/js/*.js").on("change", browserSync.reload);
-};
-
+}
 
 // Compile sass into CSS & auto-inject into browsers
 function serveSass() {
-  return src("./src/sass/*.sass")
+  return src("./src/sass/**/*.sass", "./src/sass/**/*.scss")
     .pipe(sass())
+    .pipe(
+      autoprefixer({
+        cascade: false
+      })
+    )
     .pipe(dest("./src/css"))
     .pipe(browserSync.stream());
-};
+}
 
 exports.serve = bs;
