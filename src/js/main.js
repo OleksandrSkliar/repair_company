d243ed527ca6ea91c03607833ba8e46540ecda59
Aleksandr Skliar <sklarsanja@gmail.com>
2020-03-12 15:07:44 +0300

    feat: add close modal escape and click on an empty space outside

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,25 @@
 document.addEventListener("DOMContentLoaded", function(event) {
   const modal = document.querySelector(".modal");
   const modalBtn = document.querySelectorAll("[data-togle=modal");
-  const modalClose = document.querySelector(".modal__close");
   const toggleModal = () => {
-    modal.classList.toggle("modal--visible");
+    modal.classList.add("modal--visible");
   };
+  const hideModal = () => {
+    modal.classList.remove("modal--visible");
+  };
+  const modalClose = document.querySelector(".modal__close");
   modalBtn.forEach(element => {
     element.addEventListener("click", toggleModal);
   });
-  modalClose.addEventListener("click", toggleModal);
+  modalClose.addEventListener("click", hideModal);
+  window.addEventListener("click", function(event) {
+    if (event.target == modal) {
+      hideModal();
+    }
+  });
+  document.addEventListener("keydown", function(event) {
+    if (event.code == "Escape") {
+      hideModal();
+    }
+  });
 });
